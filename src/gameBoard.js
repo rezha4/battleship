edd4e0131c71776437e8c4088d8e6db313e91f6f
rezha4 <rezha.bahari@gmail.com>
2023-09-22 15:08:07 +0700

    finished placeship, finished receiveattack placeship took 3 arg, x, y and ship receiveattack took 2 arg, if not 0/1, calls hit on occupying cell

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -13,12 +13,23 @@ class GameBoard {
         }
         return gameBoard;
     }
-    placeShip(xCoordinate, yCoordinate, shipSize) {
-        this.board[xCoordinate][yCoordinate] = 1;
+    placeShip(xCoordinate, yCoordinate, ship) {
+        for (let i = 0; i < ship.size; i++) {
+            if (ship.size > 1 && yCoordinate == 9) {
+                return;
+            } else {
+                this.board[xCoordinate][yCoordinate + i] = ship;
+            }
+        }
     }
-    //place ship function
-    //coordinate
-    //add 1 in that coordinate (ship is there)
+    //receive attack, x and y, turns that into 1. if ships in that, should call ship.hit()
+    receiveAttack(xCoordinate, yCoordinate) {
+        if (this.board[xCoordinate][yCoordinate] != 1 && this.board[xCoordinate][yCoordinate] != 0) {
+            this.board[xCoordinate][yCoordinate].hit();
+        }
+        this.board[xCoordinate][yCoordinate] = 1;
+        //how to differentiate its a ship attack or a missed attack?
+    }
 }
 
 export { GameBoard }
--- src/gameBoard.test.js
+++ src/gameBoard.test.js
@@ -1,4 +1,5 @@
 import { GameBoard } from "./gameBoard";
+import { Ships } from "./ships";
 
 test("10x10 array generated correctly", () => {
     const player = new GameBoard;
@@ -7,7 +8,36 @@ test("10x10 array generated correctly", () => {
 });
 
 test("placeShip works", () => {
+    const cruise = new Ships(5);
     const player = new GameBoard;
-    player.placeShip(5, 5, 1);
-    expect(player.board[5][5]).toBe(1);
+    player.placeShip(5, 5, cruise);
+    expect(player.board[5][5]).toBe(cruise);
+    expect(player.board[5][6]).toBe(cruise);
+    expect(player.board[5][7]).toBe(cruise);
+    expect(player.board[5][8]).toBe(cruise);
+    expect(player.board[5][9]).toBe(cruise);
+    expect(player.board[5][4]).toBe(0);
 });
+
+test("out of range placeShip is invalid, except for size 1 (which is non-existent for now)", () => {
+    const cruise = new Ships(5);
+    const player = new GameBoard;
+    player.placeShip(9, 9, cruise);
+    expect(player.board[9][9]).toBe(0);
+
+    const patrol = new Ships(1);
+    player.placeShip(9, 9, patrol);
+    expect(player.board[9][9]).toBe(patrol);
+});
+
+test("hit() works", () => {
+    const cruise = new Ships(1);
+    const player = new GameBoard;
+    player.placeShip(0, 0, cruise);
+    player.receiveAttack(0, 0);
+    expect(cruise.isSunk()).toBe(true);
+
+    player.receiveAttack(5, 5);
+    expect(player.board[5][5]).toBe(1);
+    expect(player.board[5][2]).toBe(0);
+})
